886aafc89752f9f74204c2429f838a632b4be202
subramanyam <subbuvenkat2495@gmail.com>
2021-11-25 16:17:03 +0530

    Home and Dashboard components created

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <>
-            
+            <h3 className='text-capitalize ml-2 mt-2'>
+                dashboard
+            </h3>
         </>
     )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <>
-            
+            <h1 className='text-capitalize text-center text-secondary'>
+                home page 
+            </h1>
         </>
     )
 }
