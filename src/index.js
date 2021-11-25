1343b29d1696b5c48b203c04bbac0856173da674
subramanyam <subbuvenkat2495@gmail.com>
2021-11-25 15:38:41 +0530

    index.js file updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom';
+
+import configureStore from './store/configureStore';
+
+import { Provider } from 'react-redux';
+
 import App from './Components/App';
+
+const store = configureStore();
+console.log(store.getState());
+
+// when we dispatch an action to store,we should subscribe to notify that state is updated
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,rootElement
+const jsx = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 )
+
+ReactDOM.render(jsx,rootElement);
